70b5901e6773f63076e3d712d22d397337c15375
Rock-Ayl <AnYongLiang4869@163.com>
2025-03-18 14:20:26 +0800

    修改：knife4j、各种依赖

--- src/main/java/com/rock/micro/user/controller/UserApiController.java
+++ src/main/java/com/rock/micro/user/controller/UserApiController.java
@@ -1,5 +1,7 @@
 package com.rock.micro.user.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @Author ayl
  * @Date 2022-03-23
  */
+@Api(tags = "用户模块")
 @RestController
 @RequestMapping(value = "/user")
 public class UserApiController {
 
+    @ApiOperation(value = "测试接口")
     @GetMapping(value = "/test")
     public String test() {
         return "请求成功!";
