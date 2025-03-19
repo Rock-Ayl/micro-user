6830d7c88a221381c3094f11b3b712cf25d0b1d6
Rock-Ayl <AnYongLiang4869@163.com>
2025-03-19 18:16:32 +0800

    修改：优化

--- src/main/java/com/rock/micro/user/serivce/impl/UserServiceImpl.java
+++ src/main/java/com/rock/micro/user/serivce/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.rock.micro.user.serivce.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.rock.micro.base.common.api.MyException;
 import com.rock.micro.base.common.constant.RedisKey;
 import com.rock.micro.base.data.BaseDO;
 import com.rock.micro.base.db.redis.BaseRedisService;
@@ -82,22 +83,22 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
                 .orElse("");
         //判空
         if (StringUtils.isAnyBlank(email, pwd)) {
-            //过
-            return null;
+            //抛出
+            throw new MyException(500, "邮箱、密码存在空值");
         }
         //查询是否存在当前账号
         UserDO userDO = getByEmail(email);
         //判空
         if (userDO == null) {
-            //过
-            return null;
+            //抛出
+            throw new MyException(500, "不存在邮箱");
         }
         //获取账号密码
         String userPwd = userDO.getPwd();
         //如果密码错误
         if (pwd.equals(userPwd) == false) {
-            //过
-            return null;
+            //抛出
+            throw new MyException(500, "密码不正确");
         }
         //用户id
         String userId = userDO.getId();
